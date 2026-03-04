6b081d8de0bb8d727682391fb4b8713fbe31d395
janira9345-fuente <janira9345@gmail.com>
2026-03-04 12:01:24 +0100

    actualizacion

--- queries_joinsI_II.sql
+++ queries_joinsI_II.sql
@@ -53,7 +53,14 @@ group by c.companyname, year(o.orderdate);
 --    Después de estas solicitudes desde UK y gracias a la utilidad de los resultados que se han obtenido, desde la central nos han pedido una consulta que indique 
 --     el nombre de cada compañía cliente junto con cada pedido que han realizado y su fecha.
 
-
+SELECT 
+    c.CompanyName,
+    o.OrderID,
+    o.OrderDate
+FROM Customers c
+INNER JOIN Orders o
+    ON c.CustomerID = o.CustomerID
+ORDER BY c.CompanyName, o.OrderDate;
 
 
 
@@ -65,3 +72,68 @@ group by c.companyname, year(o.orderdate);
 --     Ahora nos piden una lista con cada tipo de producto que se han vendido, sus categorías, nombre de la categoría y el nombre del producto, 
 --     y el total de dinero por el que se ha vendido cada tipo de producto (teniendo en cuenta los descuentos).
 --     Pista Necesitaréis usar 3 joins.
+
+
+
+
+-- joins parte dos--
+
+-- 2. El equipo de marketing necesita una lista con todas las categorías de productos, incluso si no tienen productos asociados.
+-- Queremos obtener el nombre de la categoría y el nombre de los productos dentro de cada categoría. 
+-- Podriamos usar un RIGTH JOIN con 'categories'?, usemos tambien la tabla 'products'
+
+SELECT * FROM products;
+
+SELECT c.categoryname,p.productname
+FROM products  AS p
+RIGHT JOIN categories AS c
+ON c.categoryid =p.categoryid; -- // me salen 77 resultados pero no hay ningun producto sin ningun resultado estara bien?
+
+-- 3  Desde el equipo de ventas nos piden obtener una lista de todos los pedidos junto con los datos de las empresas clientes. 
+--   Sin embargo, hay algunos pedidos que pueden no tener un cliente asignado.
+ --  Necesitamos asegurarnos de incluir todos los pedidos, incluso si no tienen cliente registrado.
+ 
+ SELECT * FROM orders;
+ SELECT * FROM customers;
+ 
+ SELECT o.orderid,c.companyname
+ FROM orders as o
+ LEFT JOIN customers as c
+ ON o.customerid= c.customerid;  -- me dan 830 results queremos ver si el r4esultado esta bien, no hay ningun nulo?
+SELECT COUNT(*) FROM orders;
+
+
+-- 4 El equipo de Recursos Humanos quiere saber qué empleadas han gestionado pedidos y cuáles no. 
+-- Queremos obtener una lista con todas las empleadas y, 
+-- si han gestionado pedidos, mostrar los detalles del pedido.
+SELECT 
+e. firstname,
+e. lastname,
+o.orderid
+FROM  employees e
+LEFT JOIN orders o 
+ON e.employeeid = o.employeeid;
+                                   -- // 830 results // todos los empleados han gestionado pedidos // son 9 empleados 
+
+
+
+-- 5 Desde el área de logística nos piden una lista de todos los transportistas (shippers) y los pedidos que han enviado.
+ -- Queremos asegurarnos de incluir todos los transportistas, 
+ -- incluso si no han enviado pedidos.
+ 
+SELECT * FROM shippers;
+SELECT * FROM orders;
+
+SELECT s.companyname,o.orderID
+FROM Shippers s
+LEFT JOIN orders o
+ON s.shipperid = o.shipvia;
+
+
+
+
+-- 1 Desde recursos humanos nos piden realizar una consulta que muestre por pantalla los datos de todas las empleadas y sus supervisoras.
+ -- Concretamente nos piden: la ubicación, nombre, y apellido tanto de las empleadas como de las jefas. 
+-- Investiga el resultado, ¿sabes decir quién es el director?
+
+
